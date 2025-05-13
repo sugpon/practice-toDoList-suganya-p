b181547d4699d5eed80265eaf9497ec22368ef57
Suganya Ponnusamy <suganya.sugu88@gmail.com>
2025-05-13 07:20:33 -0500

    Assignment Completed by using mp method to render an array in to a list

--- static-todo-react-app/src/components/ToDoList.jsx
+++ static-todo-react-app/src/components/ToDoList.jsx
@@ -1,12 +1,19 @@
 function ToDoList() {
-let todos= ["Learn React", "Build a project", "Read documentation"];
-return (
-    <ul className="listText">
-        <li className="todoItem">Learn React</li>
-        <li className="todoItem">Build a Project</li>
-        <li className="todoItem">Read Documentation</li>
-    </ul>
-);
+const todos= ["Learn React", "Build a project", "Read documentation"];
+    // <ul className="listText">
+    //     <li className="todoItem">Learn React</li>
+    //     <li className="todoItem">Build a Project</li>
+    //     <li className="todoItem">Read Documentation</li>
+    // </ul>
+    const todoList = todos.map((todo, index) => <li key={index}>{todo}</li>);
+    const element = <ul className="listText">
+        {todoList}
+    </ul>;
+    return(
+        <div className="todoList">
+        {element}
+        </div>
+    );
 
 }
 
